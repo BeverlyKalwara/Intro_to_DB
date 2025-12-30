66e3af013c7105602b5cd85b2705f1bae5e15597
BeverlyKalwara <beverlykluvaga@gmail.com>
2025-12-30 10:41:37 +0300

    Building a Database using a python Script

---
+++ MySQLServer.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+#creates the database alx_book_store in a MySQL Server
+
+import mysql.connector
+from mysql.connector import Error
+
+#Replace with your connection details
+try:
+    #Connect to MysqlServer
+    mydb = mysql.connector.connect(
+        host="localhost",
+        user="root",
+        password=""
+    )
+
+    mycursor = mydb.cursor()
+    #Executes sql statements using the execute() method on cursor
+    mycursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+
+    print("Database 'alx_book_store' created successfully!")
+
+except Error as e:
+    print(f"Error:{e}")
+
+finally:
+
+    #close cursor and connection to the db
+    try:
+        mycursor.close()
+        mydb.close()
+    except Exception:
+        pass
